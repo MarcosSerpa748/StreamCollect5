8741592fbb01e1043407337a832592e43e60c5c8
marcos <msrserpa@gmail.com>
2026-09-04 10:51:02 -0300

    cuida.

--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Livro {
+    private String nome;
+    private String autor;
+    private double preco;
+    private int anoLancamento;
+
+    public Livro(String nome,String autor,double preco,int anoLancamento){
+        this.nome = nome;
+        this.autor = autor;
+        this.preco = preco;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Livro livro)) return false;
+        return Objects.equals(autor, livro.autor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(autor);
+    }
+
+    @Override
+    public String toString() {
+        return  "Nome:"+this.nome+"\n"+
+                "Autor:"+this.autor+"\n"+
+                "Preço:R$"+this.preco+"\n"+
+                "Ano de lançamento:"+this.anoLancamento;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Livro> livros = List.of(
+                new Livro("A revolução do bichos","Orwell",12.99,2008),
+                new Livro("1878","Orwell",5.99,2010),
+                new Livro("Os Gregos","Asimov",59.99,2015),
+                new Livro("Os Egípcios","Asimov",45.99,2006),
+                new Livro("William Shakspear","Jennifer Basset",11.99,2018),
+                new Livro("Pinóquio","Carlo Collodi",1.99,1889),
+                new Livro("Sherlock Holmes","Arthur Conan",8.99,1959),
+                new Livro("Game of Thrones","George R. R. Martin",79.99,2007),
+                new Livro("Lord of The Rings","Tokien",5.99,1997),
+                new Livro("Hobbit","Tokien",7.99,9)
+        );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Map<String,List<Livro>> livrosPorAutor = livros
+                .stream()
+                .collect(Collectors.groupingBy(Livro::getAutor));
+
+        for (String i: livrosPorAutor.keySet()){
+            System.out.println("Existe "+livrosPorAutor.get(i).size()+" livro(s) do(a) autor(a) "+i+" na nossa biblioteca");
         }
     }
 }
